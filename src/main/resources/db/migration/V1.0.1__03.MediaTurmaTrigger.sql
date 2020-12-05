172792afb66a8a2eb6d95a0fc8388e6b0ecb5fd0
Tuwtuw <eduardoalvesvrb@hotmail.com>
2020-12-04 21:28:22 -0300

    Adição dos novos Triggers

---
+++ src/main/resources/db/migration/V1.0.1__03.MediaTurmaTrigger.sql
@@ -0,0 +1,23 @@
+create or replace function "Eldoom".media_turma_procedure() returns trigger
+    language plpgsql
+as
+$$
+BEGIN
+    UPDATE "Eldoom".turma
+    SET MediaNotaTurma= (SELECT avg(aluno_turma.NotaTotal)
+                    FROM "Eldoom".aluno_turma aluno_turma
+                    WHERE aluno_turma.Turmaid = NEW.Turmaid
+                    GROUP BY aluno_turma.turmaid
+                    LIMIT 1)
+    WHERE "Eldoom".turma.Turmaid = New.Turmaid;
+
+    RETURN NEW;
+END ;
+$$;
+
+create trigger media_turma
+    after update of NotaTotal
+    on "Eldoom".aluno_turma
+    for each row
+execute
+    procedure "Eldoom".media_turma_procedure();
---
+++ src/main/resources/db/migration/V1.0.1__05.MediaCursoTrigger.sql
@@ -0,0 +1,23 @@
+create or replace function "Eldoom".media_curso_procedure() returns trigger
+    language plpgsql
+as
+$$
+BEGIN
+    UPDATE "Eldoom".Curso
+    SET MediaNotaCurso= (SELECT avg(discip.MediaNotasDisciplina)
+                    FROM "Eldoom".Disciplina discip
+                    WHERE discip.id = NEW.id
+                    GROUP BY discip.id
+                    LIMIT 1)
+    WHERE "Eldoom".Curso.id = New.id;
+
+    RETURN NEW;
+END ;
+$$;
+
+create trigger media_curso
+    after update of MediaNotasDisciplina
+    on "Eldoom".Disciplina
+    for each row
+execute
+    procedure "Eldoom".media_Curso_procedure();
